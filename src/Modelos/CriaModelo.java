a5260eba3901e24586bbd52267cb9240a4b6db74
Ricardo Wong <rwoong11@gmail.com>
2019-10-07 17:44:59 -0600

    probando2

--- src/Modelos/CriaModelo.java
+++
@@ -1,9 +0,0 @@
-package Modelos;
-
-public class CriaModelo {
-	
-	public int registrarCria(double peso, double grasa, int color) {
-		int resultado = (int) Math.round(Math.random()); 
-		return resultado;
-	}
-}
