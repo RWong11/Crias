87352fd4c2c2be4b7e90635459cf4e1a2a47c8ee
Metri <rwoong11@gmail.com>
2019-12-03 12:47:38 -0700

    Procedimientos modificados

--- Base de datos/INSTALADOR.sql
+++ Base de datos/INSTALADOR.sql
@@ -432,36 +432,36 @@ GO
 create procedure Pa_SubirProceso @cri_id int
 AS
 BEGIN
-	DECLARE @FechaActual datetime = GETDATE()
-	DECLARE @proceso int
-	SELECT @proceso = proceso_actual FROM Vi_Crias WHERE cri_id = @cri_id
+	BEGIN TRY
+		BEGIN TRAN
+			DECLARE @FechaActual datetime = GETDATE()
+			DECLARE @proceso int
+			SELECT @proceso = proceso_actual FROM Vi_Crias WITH(UPDLOCK) WHERE cri_id = @cri_id
 
-	if(@proceso = 1)
-	BEGIN
-		IF EXISTS(SELECT cri_id FROM Vi_Crias
-					INNER JOIN Procesos_Cria ON (cri_id = pro_cria AND pro_numero = proceso_actual) 
-					WHERE cri_id = @cri_id AND est_id = 1 AND @FechaActual >= DATEADD(MINUTE/*MONTH*/, 5, pro_fechainicio))
-		BEGIN
-			BEGIN TRY
-				BEGIN TRAN
+			if(@proceso = 1)
+			BEGIN
+				IF EXISTS(SELECT cri_id FROM Vi_Crias
+							INNER JOIN Procesos_Cria ON (cri_id = pro_cria AND pro_numero = proceso_actual) 
+							WHERE cri_id = @cri_id AND est_id = 1 AND @FechaActual >= DATEADD(MINUTE/*MONTH*/, 5, pro_fechainicio))
+				BEGIN
 					UPDATE Procesos_Cria SET pro_fechaTermino = @FechaActual WHERE pro_cria = @cri_id AND pro_numero = @proceso
 					INSERT INTO Procesos_Cria (pro_cria, pro_numero, pro_fechaInicio) VALUES (@cri_id, @proceso + 1, @FechaActual)
-				COMMIT TRAN
-			END TRY
-			BEGIN CATCH
-				ROLLBACK TRAN
-			END CATCH
-		END
-	END
-	/*else if(@proceso = 2)
-	BEGIN
-	END	
-	else if(@proceso = 3)
-	BEGIN
-	END	
-	else if(@proceso = 4)
-	BEGIN
-	END	*/
+				END
+			END
+			/*else if(@proceso = 2)
+			BEGIN
+			END	
+			else if(@proceso = 3)
+			BEGIN
+			END	
+			else if(@proceso = 4)
+			BEGIN
+			END	*/
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
 END
 GO
 IF EXISTS (SELECT NAME FROM SYSOBJECTS WHERE NAME = 'Pa_AsignarSensor' AND xtype = 'P')
--- src/Modelos/BDConexion.java
+++ src/Modelos/BDConexion.java
@@ -12,7 +12,8 @@ public class BDConexion {
 
 	static synchronized public Connection getConexion(String db) {
 		if(conn == null) {
-			String url= "jdbc:sqlserver://localhost\\sqlexpress:49816;databaseName="+db+";user=sa;password=totopos;";
+			String url= "jdbc:sqlserver://localhost\\mssqlserver:1433;databaseName="+db+";user=sa;password=totopos;";
+			//String url= "jdbc:sqlserver://localhost\\sqlexpress:49816;databaseName="+db+";user=sa;password=totopos;";
 			//String url= "jdbc:sqlserver://10.10.99.113\\sqlutilerias;databaseName="+db+";user=rwong;password=456852;";
 			try {
 				conn = DriverManager.getConnection(url);
